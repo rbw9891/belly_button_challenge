a6d45fb0da400bb002ad290753734f860d6aab55
rbw9891 <rbw9891@gmail.com>
2023-03-21 12:44:41 -0400

    pseudo code

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,34 @@
+// PSEUDO CODE
+//
+//
+//
+
+// 1) read in json 
+//      - url
+//      - promise
+//      - then(function)
+
+// 2) bar chart
+//      - horizontal
+//      - x: sample_values
+//      - y: otu_ids
+//      - hover: otu_labels
+
+
+// 3) bubble chart
+//      - x: otu_ids
+//      - y: sample_values
+//      - marker size: sample_values
+//      - marker colors: otu_ids
+//      - text values: otu_labels
+
+// 4) demographic table
+//      - metadata object w/in json
+
+// 5) drop down 
+//      - init function
+//      - wrap all charts in function
+
+// Other Thoughts
+//      - html template all ready for me
+//      - 
