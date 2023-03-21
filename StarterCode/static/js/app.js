1dccd2ff9d3be92c61b1a19a7517c8c64052db02
rbw9891 <rbw9891@gmail.com>
2023-03-21 13:17:51 -0400

    d3 read json, basic horizontal chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,6 +7,16 @@
 //      - url
 //      - promise
 //      - then(function)
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// promise
+const dataPromise = d3.json(url);
+console.log(dataPromise);
+
+// get data
+d3.json(url).then(function (data) {
+    console.log(data)
+});
 
 // 2) bar chart
 //      - horizontal
@@ -14,6 +24,18 @@
 //      - y: otu_ids
 //      - hover: otu_labels
 
+// basic structure
+// not sure if text is correct for hover info
+var bar_data = [{
+    type: 'bar',
+    x: [20, 14, 23],
+    y: ['giraffes', 'orangutans', 'monkeys'],
+    text: ['label1', 'label2', 'label3'],
+    orientation: 'h'
+}];
+
+
+Plotly.newPlot("bar", bar_data)
 
 // 3) bubble chart
 //      - x: otu_ids
