5481d72808cc414e125c81a72cf37c946b1378f1
rbw9891 <rbw9891@gmail.com>
2023-03-22 16:48:31 -0400

    test bubble done, sort + filter bar graph next

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -34,6 +34,28 @@ d3.json(url).then(function (data) {
     }]
 
     Plotly.newPlot("bar", test_bar)
+
+    var test_bubble = {
+        x: id_940_otu,
+        y: id_940_sample,
+        text: id_940_labels,
+        mode: 'markers',
+        marker: {
+          color: id_940_otu,
+          size: id_940_sample
+        }
+      };
+      
+      var data = [test_bubble];
+      
+      var layout = {
+        title: 'Bubble Chart Hover Text',
+        showlegend: false,
+        height: 600,
+        width: 1200
+      };
+      
+      Plotly.newPlot("bubble", data, layout);
 });
 
 // TEST TEST TEST
@@ -71,27 +93,27 @@ d3.json(url).then(function (data) {
 //      - marker colors: otu_ids
 //      - text values: otu_labels
 
-var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 11, 12, 13],
-    text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-    mode: 'markers',
-    marker: {
-      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-      size: [40, 60, 80, 100]
-    }
-  };
+// var trace1 = {
+//     x: [1, 2, 3, 4],
+//     y: [10, 11, 12, 13],
+//     text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+//     mode: 'markers',
+//     marker: {
+//       color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+//       size: [40, 60, 80, 100]
+//     }
+//   };
   
-  var data = [trace1];
+//   var data = [trace1];
   
-  var layout = {
-    title: 'Bubble Chart Hover Text',
-    showlegend: false,
-    height: 600,
-    width: 600
-  };
+//   var layout = {
+//     title: 'Bubble Chart Hover Text',
+//     showlegend: false,
+//     height: 600,
+//     width: 600
+//   };
   
-  Plotly.newPlot("bubble", data, layout);
+//   Plotly.newPlot("bubble", data, layout);
   
 // 4) demographic table
 //      - metadata object w/in json
