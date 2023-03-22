40e252170e4bf175c737cf3f9a402114c82b2b10
rbw9891 <rbw9891@gmail.com>
2023-03-22 12:05:20 -0400

    working on json data transfer

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,6 +18,12 @@ d3.json(url).then(function (data) {
     console.log(data)
 });
 
+// variables for sample_values
+let sample_values = Object.values(data.samples)
+console.log(sample_values)
+
+
+
 // 2) bar chart
 //      - horizontal
 //      - x: sample_values
@@ -44,6 +50,28 @@ Plotly.newPlot("bar", bar_data)
 //      - marker colors: otu_ids
 //      - text values: otu_labels
 
+var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 11, 12, 13],
+    text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+    mode: 'markers',
+    marker: {
+      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+      size: [40, 60, 80, 100]
+    }
+  };
+  
+  var data = [trace1];
+  
+  var layout = {
+    title: 'Bubble Chart Hover Text',
+    showlegend: false,
+    height: 600,
+    width: 600
+  };
+  
+  Plotly.newPlot("bubble", data, layout);
+  
 // 4) demographic table
 //      - metadata object w/in json
 
