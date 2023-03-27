7914feea55af9285a33450cc9e22156d2547e814
rbw9891 <rbw9891@gmail.com>
2023-03-27 18:01:30 -0400

    update function working, need init next

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,7 +7,7 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 //  - function for dem chart 
 
 
-function makeBarChart(id, samples_dict) {
+function makeBarChart(id, data) {
 
 // -------- bar chart array variables----------
 // take the subject id that was clicked in dropdown
@@ -16,11 +16,8 @@ function makeBarChart(id, samples_dict) {
 //  - once we have that we we take that index in the list
 // make new chart
   // var samples_dict = data.samples
-  // console.log(samples_dict)
-  console.log(samples_dict)
-  console.log(typeof id)
-  console.log(id)
-  var sampleFilter = samples_dict.filter(sample => {return sample.id == id})
+
+  var sampleFilter = data.samples.filter(sample => {return sample.id == id})
   
   console.log(sampleFilter)
   //sample_values
@@ -32,6 +29,18 @@ function makeBarChart(id, samples_dict) {
   //otu_labels
   var labels_bar = sampleFilter[0].otu_labels.slice(0,10).reverse()
   console.log(labels_bar)
+
+  // -------- bubble chart array variables----------
+  var sample_values_bubble = sampleFilter[0].sample_values
+  console.log(sample_values_bubble)
+  //otu_ids
+  var ids_bubble = sampleFilter[0].otu_ids
+  console.log(ids_bubble)
+  //otu_labels
+  var labels_bubble = sampleFilter[0].otu_labels
+  console.log(labels_bubble)
+
+
 
   // -------- plot bar chart -----------
   var test_bar = [{
@@ -48,10 +57,61 @@ function makeBarChart(id, samples_dict) {
 
   Plotly.newPlot("bar", test_bar, layout_bar);
 
+  // -------- plot bubble chart -----------
+  var test_bubble = {
+    x: ids_bubble,
+    y: sample_values_bubble,
+    text: labels_bubble,
+    mode: 'markers',
+    marker: {
+      color: ids_bubble,
+      size: sample_values_bubble
+    }
+  };
+  
+  var data = [test_bubble];
+  
+  var layout = {
+    title: 'Bubble Chart Hover Text',
+    showlegend: false,
+    height: 600,
+    width: 1200
+  };
+  
+  Plotly.newPlot("bubble", data, layout);
+
 } 
 
 
+function demoGraphic(id, data) {
+  d3.select("#sample-metadata").html("");
+  var meta_ul = d3.select("#sample-metadata").append("ul");
+ 
+  var meta_dict = data.metadata.filter(d => d.id == id)[0];
 
+  
+  meta_ul.append("li").text(`Id: ${meta_dict.id}`);
+  meta_ul.append("li").text(`Ethnicity: ${meta_dict.ethnicity}`);
+  meta_ul.append("li").text(`Gender: ${meta_dict.gender}`);
+  meta_ul.append("li").text(`Age: ${meta_dict.age}`);
+  meta_ul.append("li").text(`Location: ${meta_dict.location}`);
+  meta_ul.append("li").text(`BBType: ${meta_dict.bbtype}`);
+  meta_ul.append("li").text(`WFreq: ${meta_dict.wfreq}`);
+}
+
+
+function updateTest() {
+  var subjectId = this.value
+  console.log(`updateTest ${subjectId}`);
+
+  d3.json(url).then(function (data) {
+    demoGraphic(subjectId, data);
+    makeBarChart(subjectId, data);
+  });
+  
+}
+
+d3.select("#selDataset").on("change", updateTest);
 
 // get data
 d3.json(url).then(function (data) {
@@ -61,31 +121,30 @@ d3.json(url).then(function (data) {
     var samples_dict = data.samples
     console.log(samples_dict)
     // metadata dict 
-    var meta_dict = data.metadata
-    console.log(meta_dict)
+    
+    
     // names dict
     var names_dict = data.names
     console.log(names_dict)
 
     // ------- dropdown menu --------------
     var dropDown = d3.select("#selDataset");
-    names_dict.forEach(name => dropDown.append("option").text(name).property("value", name));
+    names_dict.forEach(name => dropDown.append("option").text(name).attr("value", name));
   
 
     // ------- demographic panel -------------
     //select sample metadata element and append list of metadata
-    var meta_ul = d3.select("#sample-metadata").append("ul");
-    meta_ul.append("li").text(`Id: ${meta_dict[0].id}`);
-    meta_ul.append("li").text(`Ethnicity: ${meta_dict[0].ethnicity}`);
-    meta_ul.append("li").text(`Gender: ${meta_dict[0].gender}`);
-    meta_ul.append("li").text(`Age: ${meta_dict[0].age}`);
-    meta_ul.append("li").text(`Location: ${meta_dict[0].location}`);
-    meta_ul.append("li").text(`BBType: ${meta_dict[0].bbtype}`);
-    meta_ul.append("li").text(`WFreq: ${meta_dict[0].wfreq}`);
+   
     
     
-    // ------- bar chart function -----------
-    makeBarChart("940", samples_dict)
+    // var clickAction = d3.select("#selDataset").on("click", () => {
+    //                     let dropDown1 = d3.select("#selDataset")
+    //                     console.log(dropDown1.property("value"))
+    // })
+    // console.log(clickAction)
+
+    // // ------- bar chart function -----------
+    // makeBarChart(clickAction, samples_dict)
 
 
     // -------- bubble chart array variables----------
@@ -125,21 +184,13 @@ d3.json(url).then(function (data) {
 
 
 
-    // --------- update charts function----------
-    function updateChart() {
-      // assign value of dropdown to variable
-      let dataset = dropDown.property("value")
-
-    }
+  
 
     // paying attention to the dropdown
     //    - know which id user selects
     //    - by click get text content from option
     //    - 
-d3.select("#selDataset").on("click", () => {
-  let dropDown1 = d3.select("#selDataset")
-  console.log(dropDown1.property("value"))
-})
+
 
     // changing the charts based on the value of dropdown
 
